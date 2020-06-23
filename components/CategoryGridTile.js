cd754672d1fe33388e22a2e37861c52ed65bfee6
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-22 23:41:33 -0400

    grid styling + some configuration

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity} from 'react-native';
+
+const styles = StyleSheet.create({
+   
+  });
+  
+  export default CategoryGridTitle;
+  
