b532683c8fb74fbf192a0752402f5e2f833fc783
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-24 20:14:13 -0400

    grid

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,9 +1,53 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity} from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity, TouchableNativeFeedback, Platform, TouchableNativeFeedbackBase} from 'react-native';
+
+const CategoryGridTile = props => {
+
+  let TouchableCmp = TouchableOpacity;
+
+  if (Platform.OS === 'android' && Platform.Version >= 21){
+    TouchableCmp = TouchableNativeFeedback;
+  }
+
+  return (
+    <View style={styles.gridItem}>
+    <TouchableCmp style={{flex: 1}} onPress={() =>{props.onSelect}}>
+      <View style={{...styles.container, ...{backgroundColor: props.color}}}>
+        <Text style={styles.title}>
+          {props.title}
+        </Text>
+      </View>
+    </TouchableCmp>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-   
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
+    borderRadius: 10,
+    overflow: 'hidden'
+  }, 
+  container: {
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOpacity: 0.26,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 10,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 20,
+    color: 'white'
+  }
   });
   
-  export default CategoryGridTitle;
+  export default CategoryGridTile;
   
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,32 +1,39 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity
+} from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
-
-  const renderGridItem = (itemData) => {
+  const renderGridItem = itemData => {
     return (
-            <TouchableOpacity style={styles.gridItem} onPress={() =>{
-              props.navigation.navigate({routeName: 'CategoryMeals', params: {
-                categoryId: itemData.item.id
-              }});
-            }}>
-              <View>
-                <Text>
-                  {itemData.item.title}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          );
-    }
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: 'CategoryMeals',
+            params: {
+              categoryId: itemData.item.id
+            }
+          });
+        }}
+      />
+    );
+  };
 
   return (
     <FlatList
-    keyExtractor={(item, index) => item.id}
-    data={ CATEGORIES }
-    renderItem={renderGridItem}
-    numColumns={2}
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
     />
   );
 };
@@ -40,13 +47,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  },
-  gridItem: {
-    flex: 1,
-    margin: 15,
-    height: 150
   }
-  
 });
 
 export default CategoriesScreen;
