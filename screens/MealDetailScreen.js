89f850c642457f25b8410b5001ab020b2ecd2c8b
Dennis Basyrov <dennis.basyrov@gmail.com>
2020-06-26 00:13:24 -0400

    material bottom tabs + navigationOptions + icons

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
 
 import { MEALS } from '../data/dummy-data';
+import CustomHeaderButton from '../components/HeaderButton'
 
 
 const MealDetailScreen = props => {
@@ -22,7 +25,10 @@ MealDetailScreen.navigationOptions = (navigationData) =>{
   const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item title='Favorite' iconName='ios-star' onPress={() => {}}/>
+    </HeaderButtons>
   };
 };
 
